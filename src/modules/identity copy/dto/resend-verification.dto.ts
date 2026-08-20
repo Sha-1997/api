009f8f3ed248ee151dfbcb4bf1b3209862f9f3b3
Sha-1997 <moidheensha1997@gmail.com>
2026-08-20 14:00:31 +0400

    updated 20-aug-2026

--- src/modules/identity copy/dto/resend-verification.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class ResendVerificationDto {
-  @IsEmail({}, { message: 'Please provide a valid email address.' })
-  @IsNotEmpty({ message: 'Email address is required.' })
-  email: string;
-}
--- src/modules/identity copy/dto/send-mobile-otp.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class SendMobileOtpDto {
-
-  @IsString()
-  mobile: string;
-
-}
